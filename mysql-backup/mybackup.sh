dcaeb47895710ac29bf259adb2c5e9abd5a7607b
Konstantin Chikarev <maddcast@yandex.ru>
2020-01-07 20:06:14 +0300

    Changed dump file name format

--- mysql-backup/mybackup.sh
+++ mysql-backup/mybackup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 . /app/env.sh
-echo "Starting dump of $MYSQL_DATABASE"
 cd /backup || exit 1
-mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $MYSQL_DATABASE-$(date +%Y-%m-%d).dump
+DUMP_FILE=$MYSQL_DATABASE-$(date '+%Y-%m-%d %H-%M').dump
+echo "Dumping $MYSQL_DATABASE to file $DUMP_FILE"
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $DUMP_FILE
 if [ "$COMPRESSION" == "GZ" ]; then
-    echo "Compressing $MYSQL_DATABASE"
-    gzip $MYSQL_DATABASE-$(date +%Y-%m-%d).dump
+    echo "Compressing $DUMP_FILE"
+    gzip "$DUMP_FILE"
 fi
 echo "Removing dumps older then $DAYS_TO_KEEP days"
 find . -name '*.dump' -name '*.dump.gz' -type f -mtime +$DAYS_TO_KEEP -exec rm {} \;
