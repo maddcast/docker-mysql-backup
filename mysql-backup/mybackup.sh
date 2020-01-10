7d5b83d4b60e5f97c642e54bf5dc6fb9884e391d
Konstantin Chikarev <maddcast@yandex.ru>
2020-01-10 15:07:49 +0300

    Fixed dump file name. Fixed rotate criteria

--- mysql-backup/mybackup.sh
+++ mysql-backup/mybackup.sh
@@ -2,12 +2,12 @@
 
 . /app/env.sh
 cd /backup || exit 1
-DUMP_FILE=$MYSQL_DATABASE-$(date '+%Y-%m-%d %H-%M').dump
-echo "Dumping $MYSQL_DATABASE to file $DUMP_FILE"
+DUMP_FILE=$MYSQL_DATABASE\_$(date '+%Y-%m-%d_%H-%M').dump
+echo "Dumping database $MYSQL_DATABASE to file $DUMP_FILE"
 mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $DUMP_FILE
 if [ "$COMPRESSION" == "GZ" ]; then
     echo "Compressing $DUMP_FILE"
     gzip "$DUMP_FILE"
 fi
 echo "Removing dumps older then $DAYS_TO_KEEP days"
-find . -name '*.dump' -name '*.dump.gz' -type f -mtime +$DAYS_TO_KEEP -exec rm {} \;
+find . -type f \( -name '*.dump' -o -name '*.dump.gz' \) -mtime +$DAYS_TO_KEEP -exec rm {} \;
